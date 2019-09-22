6d711f7dbf059c85961bea59cfb7b29eb3cf6388
Eric <eric@wiseagent.com>
2019-09-22 08:59:47 -0700

    Added show/hide for InvitationalTimeline and updated eventStartDate

--- js/local_script.js
+++ js/local_script.js
@@ -1,22 +1,26 @@
-
 function getContent(filename) {
  var path = "/templates/" + filename + ".html";
  var target = "#" + filename;
  $(target).load(path);
 }
-
 function closeCalendar() {
  $("#calendar").hide();
 }
 function showCalendar() {
  $("#calendar").show();
 }
-
+function showInvitationalTimeline() {
+	$("#invitationalTimeline").show();
+	$("#showEventsTimeline").hide();
+}
+function closeInvitationalTimeline() {
+	$("#invitationalTimeline").hide();
+	$("#showEventsTimeline").show();
+}
 getContent("footer");
 getContent("calendar");
 getContent("sponsors");
-
 $(function() {
- var eventStartDate = new Date(2019, 9, 27);
+ var eventStartDate = new Date(2019, 9, 26);
  $("#event-countdown").countdown({until: eventStartDate});
 });
